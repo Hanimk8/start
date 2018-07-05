557a4b8bf87aa59777d17efafebdff994efc368c
Shitanshus <shitanshus@nextgen.com>
2018-07-05 21:14:38 +0530

    second commit

---
+++ html/javascript/firstJs.js
@@ -0,0 +1,13 @@
+function a(){
+	alert("hi ! this is some alert");
+}
+
+function b(){
+	var answer = confirm("Do you want to learn javascript ?");
+	if (answer == true){
+		window.location.href="https://www.w3schools.com/js/default.asp";
+	}
+	else{
+		alert ("Give it a look Hani !");
+	}
+}
